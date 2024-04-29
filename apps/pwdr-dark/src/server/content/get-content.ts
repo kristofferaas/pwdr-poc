a082479774177053e8de5952a4cdaa8ec0d9a5ef
Kristoffer Aas <kristoffer@aas.dev>
2024-04-29 14:08:33 +0200

    WIP

---
+++ apps/pwdr-dark/src/server/content/get-content.ts
@@ -0,0 +1,262 @@
+export async function getContent(path: string) {
+  const result = await crystallize.query(ContentQuery, {
+    language: "en",
+    path,
+  });
+}
+
+const ProductListQuery = graphql(`
+  query ProductListQuery($language: String!, $path: String!) {
+    catalogue(language: $language, path: $path) {
+      children {
+        id
+        name
+        path
+        ... on Product {
+          ...product
+          topics {
+            path
+            name
+          }
+        }
+      }
+    }
+  }
+
+  fragment content on ComponentContent {
+    ...boolean
+    ...singleLine
+    ...richText
+    ...imageContent
+    ...paragraphCollection
+    ...itemRelations
+    ...gridRelations
+    ...location
+    ...propertiesTable
+    ...dateTime
+    ...videoContent
+    ...numeric
+    ...selection
+    ...file
+  }
+
+  fragment component on Component {
+    id
+    name
+    type
+    content {
+      ...content
+      ...componentChoice
+      ...contentChunk
+    }
+  }
+
+  fragment dateTime on DatetimeContent {
+    datetime
+  }
+
+  fragment gridRelations on GridRelationsContent {
+    grids {
+      id
+      name
+    }
+  }
+
+  fragment imageContent on ImageContent {
+    images {
+      ...image
+    }
+  }
+
+  fragment image on Image {
+    url
+    altText
+    key
+    variants {
+      url
+      width
+      key
+    }
+  }
+
+  fragment itemRelations on ItemRelationsContent {
+    items {
+      id
+      name
+      path
+    }
+    productVariants {
+      sku
+      name
+    }
+  }
+
+  fragment location on LocationContent {
+    lat
+    long
+  }
+
+  fragment paragraphCollection on ParagraphCollectionContent {
+    paragraphs {
+      body {
+        ...richText
+      }
+    }
+  }
+
+  fragment product on Product {
+    id
+    name
+    type
+    language
+    path
+
+    components {
+      ...component
+    }
+
+    variants {
+      name
+      sku
+      components {
+        ...component
+      }
+      price
+      priceVariants {
+        identifier
+        name
+        price
+        currency
+      }
+      stockLocations {
+        identifier
+        name
+        stock
+      }
+      isDefault
+      images {
+        url
+        altText
+        key
+
+        variants {
+          key
+          width
+          url
+        }
+      }
+
+      subscriptionPlans {
+        identifier
+        name
+        periods {
+          id
+          name
+          initial {
+            priceVariants {
+              identifier
+              name
+              price
+              currency
+            }
+          }
+          recurring {
+            priceVariants {
+              identifier
+              name
+              price
+              currency
+            }
+          }
+        }
+      }
+    }
+
+    vatType {
+      name
+      percent
+    }
+  }
+
+  fragment propertiesTable on PropertiesTableContent {
+    sections {
+      ... on PropertiesTableSection {
+        title
+        properties {
+          key
+          value
+        }
+      }
+    }
+  }
+
+  fragment richText on RichTextContent {
+    plainText
+  }
+
+  fragment boolean on BooleanContent {
+    value
+  }
+
+  fragment singleLine on SingleLineContent {
+    text
+  }
+
+  fragment videoContent on VideoContent {
+    videos {
+      ...video
+    }
+  }
+
+  fragment video on Video {
+    id
+    playlists
+    title
+    thumbnails {
+      ...image
+    }
+  }
+
+  fragment numeric on NumericContent {
+    number
+    unit
+  }
+
+  fragment componentChoice on ComponentChoiceContent {
+    selectedComponent {
+      id
+      name
+      type
+      content {
+        ...content
+      }
+    }
+  }
+
+  fragment contentChunk on ContentChunkContent {
+    chunks {
+      id
+      name
+      type
+      content {
+        ...content
+      }
+    }
+  }
+
+  fragment selection on SelectionContent {
+    options {
+      key
+      value
+    }
+  }
+
+  fragment file on FileContent {
+    files {
+      url
+      key
+      title
+      size
+    }
+  }
+`);
